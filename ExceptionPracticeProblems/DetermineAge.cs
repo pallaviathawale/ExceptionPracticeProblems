ff090e981b3c5d081b74f89cabd90b93f3cb04e2
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-30 17:16:10 +0530

    Add: File

---
+++ ExceptionPracticeProblems/DetermineAge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPracticeProblems
+{
+    public class DetermineAge
+    {
+        public void GetCategory(int age)
+        {
+            if (age == null || age < 1)
+            {
+                Console.WriteLine("Invalid age entered.");
+            }
+
+            if (age >= 1 && age <= 14)
+            {
+                Console.WriteLine("Category: Children");
+            }
+            else if (age >= 15 && age <= 24)
+            {
+                Console.WriteLine("Category: Youth");
+            }
+            else if (age >= 25 && age <= 64)
+            {
+                Console.WriteLine("Category: Adults");
+            }
+            else
+            {
+                Console.WriteLine("Category: Seniors");
+            }
+        }
+    }
+}
---
+++ ExceptionPracticeProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExceptionPracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            DetermineAge determineAge = new DetermineAge();
+            determineAge.GetCategory(age);
+        }
+    }
+}
